dcf3cc2dc08f4916544e646051b396013500920b
alan <saxal28@gmail.com>
2017-02-01 21:32:23 -0600

    added test cases, added all routes. the patch test fails even though it succeeds in postman....it seems to be a step behind

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+const {ObjectID} = require("mongodb");
 const {db} = require("./db/db");
-const { Note } = require("./models/Note");
+const {Note} = require("./models/Note");
+const _ = require("lodash");
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const colors = require("colors");
 
 app.use(bodyParser.json());
 
@@ -22,7 +25,6 @@ app.get("/notes", (req, res) => {
 });
 
 app.post("/notes", (req, res) => {
-  console.log(req.body ? true : false)
   var newNote = new Note({
     title: req.body.title,
     author: req.body.author,
@@ -32,19 +34,55 @@ app.post("/notes", (req, res) => {
   })
 
   newNote.save().then(note => {
-    console.log(note)
     res.send(note)
   }).catch(e => res.status(400).send({}));
 })
 
-// app.get("/notes/:id", (req, res) => {
-//
-// })
+app.get("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({err: "ObjectID is not Valid"});
+  }
 
-app.listen(PORT, () => {
-  console.log(`App started on port ${PORT}`)
+  Note.findById(id).then(note => {
+    if(!note) {
+      return res.status(400).send({err: "Note Not Found/Doesnt Exist"})
+    }
+    res.send(note);
+  }).catch(e => res.send({e}));
 });
 
+app.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({err: "ObjectID is not Valid"});
+  }
+
+  Note.findByIdAndRemove(id).then(note => {
+    if(!note) {
+      return res.status(400).send({err: "Note Not Found/Doesnt Exist"})
+    }
+    res.send(note);
+  }).catch(e => res.send({e}));
+})
+
+app.patch("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({err: "ObjectID is not Valid"});
+  }
+
+  var body = _.pick(req.body, ["title", "summary", "link", "body"]);
+  console.log(body)
+
+  Note.findByIdAndUpdate(id, {$set: body}).then(note => {
+    res.send(note);
+  }).catch(e => res.status(400).send({err: "Unable to Find Note to Update"}));
+})
+
+app.listen(PORT, () => {
+  console.log(`\nApp started on port ${PORT}\n`.white.bgGreen.bold)
+});
 
 //for mocha tests
 module.exports = {
--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 const expect = require("expect");
 const request = require("supertest");
 const {app} = require("../app");
+const {Note} = require("../models/Note");
+const {seedDB, testNote, testNote2} = require("../seed");
+const {ObjectID} = require("mongodb");
+const colors = require("colors");
 
 describe("this is the initial test", () => {
   it("4 should equal 4", () => {
@@ -8,15 +12,64 @@ describe("this is the initial test", () => {
   })
 });
 
-describe("GET /posts", () => {
-  it("it should render a list of posts", (done) => {
+describe("Seeding the DB...", () => {
+  //mainly for visual purposes.... :)
+  it("deleting all notes...", () => {
+    seedDB();
+  });
+  it('adding two test notes...', () => {
+
+  })
+
+})
+
+describe("GET /notes", () => {
+  it("should render all notes", (done) => {
     request(app)
-    .get("/notes")
-    .expect(200)
-    .then(res => {
-      console.log(res.body);
-      expect(res.body).toBe(2);
-    }).catch(e => console.log(e))
-    done();
+      .get("/notes")
+      .expect(200)
+      .expect(res => {
+        expect(res.body).toNotBe({});
+      })
+    .end(done)
+  })
+})
+
+describe("GET /notes/:id", () => {
+  it('should get a note by id', (done) => {
+    var id = testNote._id;
+    request(app)
+      .get(`/notes/${id}`)
+      .expect(200)
+      .end(done)
+  })
+})
+
+describe("PATCH /notes/:id", () => {
+  it("should update a note by id", done => {
+    var id = testNote2._id;
+    request(app)
+      .get(`/notes/${id}`)
+      .expect(200)
+      .end(() => {
+        Note.findByIdAndUpdate(id, {$set:{title: "Just Updated the Note"}}).then(note => {
+          expect(note._id).toEqual(testNote2._id);
+          //updates in postman but the test doesnt pass......
+          //async issue???
+          console.log("updates in postman but the test doesnt pass......".red);
+          expect(note.title).toNotBe(testNote2.title);
+          done()
+        }).catch(e => done(e))
+      })
+  })
+})
+
+describe("DELETE /notes/:id", () => {
+  it("should delete a note by id", (done) => {
+    var id = testNote._id;
+    request(app)
+      .delete(`/notes/${id}`)
+      .expect(200)
+      .end(done)
   })
 })
