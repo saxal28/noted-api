6da97442c42fe383922e8873ba7e78a7257f167c
alan <saxal28@gmail.com>
2017-02-02 00:02:00 -0600

    bug fix for coors

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://grave-digger-lorna-16454.netlify.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'http://grave-digger-lorna-16454.netlify.com/');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
