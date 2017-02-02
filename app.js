8b6273eea2534fba4c36fdca0126662332ecda3a
alan <saxal28@gmail.com>
2017-02-02 00:06:01 -0600

    bug fix for coors

--- app.js
+++ app.js
@@ -15,8 +15,10 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    process.env.PORT ?
     res.setHeader('Access-Control-Allow-Origin', 'http://grave-digger-lorna-16454.netlify.com/');
+    :
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
